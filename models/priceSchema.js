71c97d2ea40554590445bbc543b20b7d5b5f9f30
Bishal Bhatta <bishalbhatta1028@gamil.com>
2024-05-24 21:38:44 +0545

    update

--- models/priceSchema.js
+++ models/priceSchema.js
@@ -2,20 +2,19 @@
 
 const mongoose = require("mongoose");
 
-const priceUpdateSchema = new mongoose.Schema({
-  region: {
-    type: String,
-    required: true,
+const priceUpdateSchema = new mongoose.Schema(
+  {
+    region: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 const PriceUpdate = mongoose.model("PriceUpdate", priceUpdateSchema);
 
